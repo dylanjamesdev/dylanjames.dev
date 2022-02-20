f93984f1eeef5c0145a4b12ced5eaa27e79b2d85
Dylan J <dylanjames420@protonmail.com>
2022-02-20 07:50:32 -0500

    uwu

--- containers/Github.jsx
+++ containers/Github.jsx
@@ -4,30 +4,29 @@ import GithubCard from "../components/RepoCard";
 import { Fade } from "react-reveal";
 
 const Projects = ({ repos }) => {
-	
-	return (
-		<section className="section section-lg">
-			<Container>
-				<Fade bottom duration={1000} distance="40px">
-					<div className="d-flex p-4">
-						<div>
-							<div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
-								<i className="ni ni-laptop text-info" />
-							</div>
-						</div>
-						<div className="pl-4">
-							<h4 className="display-3 text-info">Public Repos</h4>
-						</div>
-					</div>
-					<Row className="row-grid align-items-center">
-						{repos.map((data, i) => {
-							return <GithubCard key={i} data={data} />;
-						})}
-					</Row>
-				</Fade>
-			</Container>
-		</section>
-	);
+  return (
+    <section className="section section-lg">
+      <Container>
+        <Fade bottom duration={1000} distance="40px">
+          <div className="d-flex p-4">
+            <div>
+              <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
+                <i className="ni ni-laptop text-info" />
+              </div>
+            </div>
+            <div className="pl-4">
+              <h4 className="display-3 text-info">Public Repos</h4>
+            </div>
+          </div>
+          <Row className="row-grid align-items-center">
+            {repos.map((data, i) => {
+              return <GithubCard key={i} data={data} />;
+            })}
+          </Row>
+        </Fade>
+      </Container>
+    </section>
+  );
 };
 
 export default Projects;
